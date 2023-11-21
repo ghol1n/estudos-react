6d844a409890522a8a61cf4e2a6d123479eeecf2
ghol1n <gabrielbertholin@gmail.com>
2023-11-21 14:15:15 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Produto from './Produto';
 
 const App = () => {
-  const [json, setJson] = useState({ value: null, isActive: false });
+  const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
 
-  async function fetchProduto(url) {
-    setJson({ value: null, isActive: false });
-    const response = await fetch(url);
-    const data = await response.json();
-    setJson({ value: data, isActive: true });
+  async function handleClick(event) {
+    setCarregando(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    const json = await response.json();
+    setDados(json);
+    setCarregando(false);
   }
 
   return (
-    <div>
-      <button
-        onClick={() =>
-          fetchProduto('https://ranekapi.origamid.dev/json/api/produto/tablet')
-        }
-      >
-        Tablet
-      </button>
-      <button
-        onClick={() =>
-          fetchProduto(
-            'https://ranekapi.origamid.dev/json/api/produto/smartphone',
-          )
-        }
-      >
-        Smartphone
-      </button>
-      <button
-        onClick={() =>
-          fetchProduto(
-            'https://ranekapi.origamid.dev/json/api/produto/notebook',
-          )
-        }
-      >
-        Notebook
-      </button>
-      {!json.isActive && <div>Carregando...</div>}
-      {json.isActive && json.value && (
-        <div>
-          <h1>{json.value.nome}</h1>
-          <p>{json.value.descricao}</p>
-        </div>
-      )}
-    </div>
+    <>
+      <button onClick={handleClick}>smartphone</button>
+      <button onClick={handleClick}>tablet</button>
+      <button onClick={handleClick}>notebook</button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados} />}
+    </>
   );
 };
 
