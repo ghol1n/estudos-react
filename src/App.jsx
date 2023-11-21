b1152a389163638596d32408f16d69a5827584b7
ghol1n <gabrielbertholin@gmail.com>
2023-11-21 12:58:39 -0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,49 @@
 import React, { useState } from 'react';
-import Home from './Home';
-import Produtos from './Produtos';
 
 const App = () => {
-  const [path, setPath] = useState(window.location.pathname);
+  const [json, setJson] = useState({ value: null, isActive: false });
 
-  const handleNavigation = (newPath) => {
-    setPath(newPath);
-    window.history.pushState({}, '', newPath);
-  };
+  async function fetchProduto(url) {
+    setJson({ value: null, isActive: false });
+    const response = await fetch(url);
+    const data = await response.json();
+    setJson({ value: data, isActive: true });
+  }
 
   return (
     <div>
-      {/* Botões de navegação */}
-      <nav>
-        <ul>
-          <li>
-            <button onClick={() => handleNavigation('/')}>Home</button>
-          </li>
-          <li>
-            <button onClick={() => handleNavigation('/produtos')}>
-              Produtos
-            </button>
-          </li>
-        </ul>
-      </nav>
-
-      {path === '/' ? <Home /> : <Produtos />}
+      <button
+        onClick={() =>
+          fetchProduto('https://ranekapi.origamid.dev/json/api/produto/tablet')
+        }
+      >
+        Tablet
+      </button>
+      <button
+        onClick={() =>
+          fetchProduto(
+            'https://ranekapi.origamid.dev/json/api/produto/smartphone',
+          )
+        }
+      >
+        Smartphone
+      </button>
+      <button
+        onClick={() =>
+          fetchProduto(
+            'https://ranekapi.origamid.dev/json/api/produto/notebook',
+          )
+        }
+      >
+        Notebook
+      </button>
+      {!json.isActive && <div>Carregando...</div>}
+      {json.isActive && json.value && (
+        <div>
+          <h1>{json.value.nome}</h1>
+          <p>{json.value.descricao}</p>
+        </div>
+      )}
     </div>
   );
 };
